9fbe928618d162e21f31bb8ff7be21f2e9e027ca
morris <akunnemorris@gmail.com>
2024-07-27 18:43:12 +0100

    added info class

--- src/card.js
+++ src/card.js
@@ -12,9 +12,9 @@ function Card(props) {
             className="circle-img"
           />
         </div>
-        <div className="bottom">
-          <p>{props.phone}</p>
-          <p>{props.email}</p>
+        <div className="bottom" >
+          <p className="info">{props.phone}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div> 
     );
