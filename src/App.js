681e33e3ae336a63e9b3fec5df4a492e9696d844
morris <akunnemorris@gmail.com>
2024-08-14 15:05:07 +0100

    React useState HOOKS

--- src/App.js
+++ src/App.js
@@ -2,40 +2,46 @@ import React from "react";
 import { contacts } from "./contacts";
 import {Card} from "./card";
 
-function createcard(props){
-  return <Card 
-  key = {props.id}
-  name= {props.name}
-  img = {props.imgURL}
-  phone = {props.phone}
-  email = {props.email}
-  />
-}
+// function createcard(props){
+//   return <Card 
+//   key = {props.id}
+//   name= {props.name}
+//   img = {props.imgURL}
+//   phone = {props.phone}
+//   email = {props.email}
+//   />
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className="heading">My Contacts</h1>
+//       {contacts.map(createcard)}
+    
+import React, { useState } from "react";
 
 function App() {
+  const [count, setcount] = useState(time);
+
+  function time() {
+    return new Date().toLocaleTimeString();
+  }
+  
+  function gettime() {
+    setcount(time);
+  }
+  setInterval(gettime, 1000);
+  
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      {contacts.map(createcard)}
-    
-      {/* < Card     
-      name= {contacts[0].name}
-      img ={contacts[0].imgURL}
-      phone = {contacts[0].phone}
-      email = {contacts[0].email }
-      />
-       <Card 
-      name= {contacts[1].name}
-      img ={contacts[1].imgURL}
-      phone = {contacts[1].phone}
-      email = {contacts[1].email }
-      />
-       <Card 
-      name= {contacts[2].name}
-      img ={contacts[2].imgURL}
-      phone = {contacts[2].phone}
-      email = {contacts[2].email }
-      /> */}
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={gettime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
+
     </div>
   );
 }
